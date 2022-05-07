977a8dd0f58aea7947f025b6b12348fb12964529
Jojo-pft <104636159+Jojo-pft@users.noreply.github.com>
2022-05-07 13:41:44 +0200

    versucht bug zu beheben

--- Galaxy SFz/Assets/Assets/script/EnemyFollow2.cs
+++ Galaxy SFz/Assets/Assets/script/EnemyFollow2.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyFollow2 : MonoBehaviour{
+   
+public float speed;
+private Transform target;
 
-    public float speed;
-    private Transform target;
-
-    void Start(){
+    void Start()
+    {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    void Update(){
-        if(Vector2.Distance(transform.position, target.position) > 2){
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > 2)
+        {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
     }
